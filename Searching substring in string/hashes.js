ddbfbda15b3b5253f0f2d9d68b0860e8f45383b4
Andrey Baryshev (Barkos) <andrey.bar66@gmail.com>
2023-11-30 19:17:10 +0500

    Update D30M11Y23.19
    hashes.js:
    Изменение поиска ключа '-n' и получение следующего значения

--- Searching substring in string/hashes.js
+++ Searching substring in string/hashes.js
@@ -45,9 +45,9 @@ module.exports.sum = async(keys, stringFile, substringFile) => {
     time = performance.now() - time; // Конец работы алгоритма
     if (keys.indexOf('-c') != -1) console.log(`Collision: ${counterCollision}`);
     if (keys.indexOf('-t') != -1) console.log(`Time: ${time.toFixed(4)}ms`); // Если был найден ключ на вывод времени работы кода
-    if (keys.some(element => element.startsWith('-n'))) { // Если был найден ключ на вывод N элементов
-        let number = parseInt(keys[keys.findIndex(element => element.startsWith('-n')) + 1]); // Получаем следующий элемент. Это количество, сколько элементов надо вывести
-        if (number < 0) return console.log('The number of elements per output must be at least 0'); // Ошибка, если количество меньше 0
+    if (keys.indexOf('-n') != -1) { // Если был найден ключ на вывод N элементов
+        let number = parseInt(keys[keys.indexOf('-n') + 1]); // Получаем следующий элемент. Это количество, сколько элементов надо вывести
+        if (number < 0 || isNaN(number)) return console.log('The number of elements per output must be at least 0'); // Ошибка, если количество меньше 0
         console.log(arrayIndex.slice(0, number));
     } else console.log(arrayIndex); // Если не был найден ключ не вывод определённого кол-ва элементов
 }
@@ -89,9 +89,9 @@ module.exports.sumSquare = async(keys, stringFile, substringFile) => {
     time = performance.now() - time; // Конец работы алгоритма
     if (keys.indexOf('-c') != -1) console.log(`Collision: ${counterCollision}`);
     if (keys.indexOf('-t') != -1) console.log(`Time: ${time.toFixed(4)}ms`); // Если был найден ключ на вывод времени работы кода
-    if (keys.some(element => element.startsWith('-n'))) { // Если был найден ключ на вывод N элементов
-        let number = parseInt(keys[keys.findIndex(element => element.startsWith('-n')) + 1]); // Получаем следующий элемент. Это количество, сколько элементов надо вывести
-        if (number < 0) return console.log('The number of elements per output must be at least 0'); // Ошибка, если количество меньше 0
+    if (keys.indexOf('-n') != -1) { // Если был найден ключ на вывод N элементов
+        let number = parseInt(keys[keys.indexOf('-n') + 1]); // Получаем следующий элемент. Это количество, сколько элементов надо вывести
+        if (number < 0 || isNaN(number)) return console.log('The number of elements per output must be at least 0'); // Ошибка, если количество меньше 0
         console.log(arrayIndex.slice(0, number));
     } else console.log(arrayIndex); // Если не был найден ключ не вывод определённого кол-ва элементов
 }
@@ -134,9 +134,9 @@ module.exports.rabinaKarp = async(keys, stringFile, substringFile) => {
     time = performance.now() - time; // Конец работы алгоритма
     if (keys.indexOf('-c') != -1) console.log(`Collision: ${counterCollision}`);
     if (keys.indexOf('-t') != -1) console.log(`Time: ${time.toFixed(4)}ms`); // Если был найден ключ на вывод времени работы кода
-    if (keys.some(element => element.startsWith('-n'))) { // Если был найден ключ на вывод N элементов
-        let number = parseInt(keys[keys.findIndex(element => element.startsWith('-n')) + 1]); // Получаем следующий элемент. Это количество, сколько элементов надо вывести
-        if (number < 0) return console.log('The number of elements per output must be at least 0'); // Ошибка, если количество меньше 0
+    if (keys.indexOf('-n') != -1) { // Если был найден ключ на вывод N элементов
+        let number = parseInt(keys[keys.indexOf('-n') + 1]); // Получаем следующий элемент. Это количество, сколько элементов надо вывести
+        if (number < 0 || isNaN(number)) return console.log('The number of elements per output must be at least 0'); // Ошибка, если количество меньше 0
         console.log(arrayIndex.slice(0, number));
     } else console.log(arrayIndex); // Если не был найден ключ не вывод определённого кол-ва элементов
 }
